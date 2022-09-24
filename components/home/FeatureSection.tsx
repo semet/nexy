2b7161691e3545ad83ea02f1c413fd81aaa5b8e9
Semet <hamdanilombok@gmail.com>
2022-09-24 12:57:11 +0800

    make it full-width on mobile

--- components/home/FeatureSection.tsx
+++ components/home/FeatureSection.tsx
@@ -24,9 +24,9 @@ const FeatureSection = () => {
 	return (
 		<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 place-items-center -mt-14 px-4 py-6 lg:px-20 bg-gray-100">
 			{features.map((feature, index) => (
-				<div className="relative" key={index}>
+				<div className="relative w-full" key={index}>
 					<div className="absolute -inset-1 bg-blue-700 blur-md"></div>
-					<div className="relative flex flex-col justify-center space-y-6 text-lg w-full items-center p-4 bg-gray-50 hover:bg-gradient-to-br hover:from-[#6059e7] hover:to-emerald-200 text-gray-700 hover:text-white shadow-lg rounded-xl hover:shadow-xl hover:-mt-2 z-10 cursor-pointer transition-all ease-in-out duration-300">
+					<div className="relative flex flex-col justify-center space-y-6 text-lg items-center p-4 bg-gray-50 hover:bg-gradient-to-br hover:from-[#6059e7] hover:to-emerald-200 text-gray-700 hover:text-white shadow-lg rounded-xl hover:shadow-xl hover:-mt-2 z-10 cursor-pointer transition-all ease-in-out duration-300">
 						<feature.icon className="text-4xl " />
 						<p className="text-center">{feature.text}</p>
 					</div>
