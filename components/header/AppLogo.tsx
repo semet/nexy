b4ae040d731b0d509f7b527c4d3e552d5a8ad5c4
Semet <hamdanilombok@gmail.com>
2022-09-28 17:50:40 +0800

    include link in logo

--- components/header/AppLogo.tsx
+++ components/header/AppLogo.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import React from "react";
 
 const AppLogo = () => {
 	return (
-		<div>
-			{/* Logo Large */}
-			<img
-				className="w-[150px] hidden lg:inline-flex"
-				src="/images/logo/nexy-2.png"
-				alt="Nexy"
-			/>
-			{/* Logo Small */}
-			<img
-				className="w-[100px] inline-flex lg:hidden"
-				src="/images/logo/nexy.png"
-				alt="Nexy"
-			/>
-		</div>
+		<Link href="/">
+			<a>
+				{/* Logo Large */}
+				<img
+					className="w-[150px] hidden lg:inline-flex"
+					src="/images/logo/nexy-2.png"
+					alt="Nexy"
+				/>
+				{/* Logo Small */}
+				<img
+					className="w-[100px] inline-flex lg:hidden"
+					src="/images/logo/nexy.png"
+					alt="Nexy"
+				/>
+			</a>
+		</Link>
 	);
 };
 
