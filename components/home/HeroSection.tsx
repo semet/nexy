040ccb6173477aa0d98bcad650d748a8900730f5
Semet <hamdanilombok@gmail.com>
2022-09-29 00:08:15 +0800

    fix github link

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -11,10 +11,15 @@ const HeroSection = () => {
 				<h2 className="ml-2 font-semibold italic text-xl lg:text-3xl text-gray-800 tracking-wide">
 					enjoy the simplicity
 				</h2>
-				<button className="px-3 lg:px-5 py-2 lg:py-3 flex justify-between items-center space-x-4 bg-gradient-to-r from-[#564fd2] to-[#2784a9b5] hover:bg-gradient-to-r hover:from-[#2784a9b5] hover:to-[#564fd2] text-white shadow-lg hover:shadow-xl rounded-3xl">
-					<span className="text-md lg:text-lg">Fork me</span>
+				<a
+					className="px-3 lg:px-5 py-2 lg:py-3 flex justify-between items-center space-x-4 bg-gradient-to-r from-[#564fd2] to-[#2784a9b5] hover:bg-gradient-to-r hover:from-[#2784a9b5] hover:to-[#564fd2] text-white shadow-lg hover:shadow-xl rounded-3xl"
+					href="https://github.com/semet"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<span className="text-md lg:text-lg">Fork</span>
 					<FiGithub className="text-md lg:text-2xl" />
-				</button>
+				</a>
 			</div>
 		</div>
 	);
